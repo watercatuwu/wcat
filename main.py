25a0667048b87edaa5c7b11ab8207269ebb0af85
watercatuwu <92051934+watercatuwu@users.noreply.github.com>
2021-11-21 05:02:32 +0800

    update#5

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
 from pytube import YouTube
 import os
+import schedule
 import download as dl
 
+#schedule
+def delfile():
+    path  = 'assets/video'
+    files_in_dir = os.listdir(path)
+
+    for file in files_in_dir:
+        os.remove(f'{path}/{file}')
+    os.rmdir(path) 
+schedule.every().day.at('00:00').do(delfile)
 
 #server(flask)
 app = Flask(__name__, static_folder="./assets")
@@ -17,6 +27,16 @@ def submit():
     yt= YouTube(url)
     dl.mp3(url)
     return render_template('yt_result.html',author=yt.author,title=yt.title,thumbnail=yt.thumbnail_url,mp3=F"video/{yt.title}.mp3",mp4=F"video/{yt.title}.mp4")
+
+@app.route('/dashboard')
+def dashboard():
+    file_size = 0
+    path = 'assets/video'
+    for fileName in os.listdir(path):
+        file_size += os.path.getsize(path+'/'+fileName)
+    file_size = round(file_size/1048576, 2)
+    print(file_size, "mb")
+    return render_template('dashboard.html',file_size=file_size)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
